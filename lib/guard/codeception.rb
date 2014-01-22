f4339fbb8d12b676473a378a72613dae0191027f
Thomas Muntaner <thomas.muntaner@gmail.com>
2014-01-22 12:00:19 +0100

    Fixed formatting

--- lib/guard/codeception.rb
+++ lib/guard/codeception.rb
@@ -2,34 +2,35 @@ require 'guard'
 require 'guard/plugin'
 
 module Guard
-	class Codeception < Plugin
-		
-		autoload :Runner, 'guard/codeception/runner'
+  class Codeception < Plugin
 
-		DEFAULT_OPTIONS = {
-			:test_on_start 	=> false,
-			:suites			=> [:acceptance, :functional, :unit],
-			:debug 			=> false,
-			:groups 		=> [],
-			:codecept 		=> 'codecept',
-			:cli			=> false
-		}
+    autoload :Runner, 'guard/codeception/runner'
 
-		def initialize(options = {})
-			@options = DEFAULT_OPTIONS.merge(options)
-			super(@options)
-		end
-		
-		def start
-			run if options[:test_on_start]
-		end
-		
-		def run_on_change(paths)
-			 run
-		end
-	
-		def run
-			Runner.run options
-		end
-	end
+    DEFAULT_OPTIONS = {
+      test_on_start:  false,
+      suites:         [:acceptance, :functional, :unit],
+      debug:          false,
+      groups:         [],
+      codecept:       'codecept',
+      cli:            false
+    }
+
+    def initialize(options = {})
+      @options = DEFAULT_OPTIONS.merge(options)
+      super(@options)
+    end
+
+    def start
+      run if options[:test_on_start]
+    end
+
+    def run_on_change(paths)
+      run
+    end
+
+    def run
+      Runner.run options
+    end
+
+  end
 end
