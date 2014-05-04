8bd8508c575d6fa51770569bcf2f1d197c164f72
Colby Swandale <colby@taplaboratories.com.au>
2014-05-04 23:48:30 +1000

    2 new tests to make sure the notifciation parser is working for passed and failed tests for codeception

--- spec/guard/codeception/parser_spec.rb
+++ spec/guard/codeception/parser_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Guard::Codeception::Parser do
 
   describe '#parse' do
+
     it 'should give 0 if nothing exists in the tests' do
       results = subject.parse('')
       results.each do |key, value|
@@ -10,11 +11,32 @@ describe Guard::Codeception::Parser do
       end
     end
 
+    context 'when codecept tests pass' do
+
+      let (:codecept) { IO.read(Dir.getwd + '/spec/fixtures/results/codeception_success') }
+
+      it 'parses the codecept run results' do
+        result = subject.parse(codecept)
+        expect(result).to eq({tests: 2, assertions: 2, failures: 0, errors: 0})
+      end
+    end
+
+    context 'when codecept tests fail' do
+
+      let (:codecept) { IO.read(Dir.getwd + '/spec/fixtures/results/codeception_failure') }
+
+      it 'parses the codecept run results' do
+          result = subject.parse(codecept)
+          expect(result).to eq({tests: 2, assertions: 2, failures: 1, errors: 0})
+      end
+
+    end
+
     it 'should give values if they exists' do
       keys = [:tests, :assertions, :failures, :errors]
 
       keys.each do |key|
-        results = subject.parse("foo #{key.to_s.capitalize}: 42 bar")
+        results = subject.parse("OK #{key.to_s.capitalize}: 42 bar")
         results[key].should eq(42)
       end
 
